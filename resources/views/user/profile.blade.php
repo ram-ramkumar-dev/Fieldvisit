e1057ea8a0aa60e94ce24f020938b15818094f8c
ram <mramkumar.ram@gmail.com>
2024-08-30 11:40:53 +0800

    change in progfile

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -12,7 +12,8 @@
                   <h3 class="font-600 text-white text-center mb-0" style="text-transform:uppercase">{{ $user->name }} </h3>
                   <p class="text-white text-center mb-5" style="text-transform:uppercase">{{ $user->company_name }}</p>
                </div>
-               <?php if(isset($user->email)){ ?><div class="pro-content rounded">
+               <div class="pro-content rounded">
+               <?php if(isset($user->email)){ ?>
                   <div class="d-flex align-items-center mb-3">
                      <div class="p-icon mr-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -22,14 +23,15 @@
                      <p class="mb-0 eml">{{ ucfirst($user->email) }}</p>
                   </div>
                   <?php } ?>
+                  <?php if(isset($user->phone_number)){ ?>
                   <div class="d-flex align-items-center mb-3">
                      <div class="p-icon mr-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"></path>
                         </svg>
                      </div>
-                     <p class="mb-0"></p>
-                  </div>
+                     <p class="mb-0">{{ $user->phone_number }}</p>
+                  </div><?php } ?>
                   <div class="d-flex align-items-center mb-3">
                      <div class="p-icon mr-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
