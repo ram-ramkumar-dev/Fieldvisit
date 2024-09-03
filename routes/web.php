8ca07e8f5a467cee9850b8c286a89b9f647f7b2f
ram <mramkumar.ram@gmail.com>
2024-09-03 16:09:07 +0800

    web.php

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,17 @@ use Illuminate\Support\Facades\Route;
 */ 
 
 Route::get('/', [UserController::class, 'home'])->name('home');
-
+Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
 Route::middleware(['auth'])->group(function () {
+    Route::resource('batches', BatchesController::class);
+    Route::resource('clientgroups', ClientGroupController::class);
+    Route::resource('clients', ClientController::class);
+    Route::resource('drivers', DriverController::class);
+    Route::resource('statuses', StatusController::class);
+    Route::get('streetmap', [DriverController::class, 'streetmap'])->name('drivers/streetmap');
     Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard'); 
     Route::get('profile', [UserController::class, 'profile'])->name('profile');
     //Route::get('/users', [UserController::class, 'users'])->name('users');
@@ -31,8 +40,6 @@ Route::middleware(['auth'])->group(function () {
     Route::post('password', [UserController::class, 'password_action'])->name('password.action');
     Route::get('logout', [UserController::class, 'logout'])->name('logout');
 
-    // Additional routes with permission checks
-   // Route::get('reports', [UserController::class, 'reports'])->name('reports')->middleware('check.permissions:reports');
     Route::get('settings', [UserController::class, 'settings'])->name('settings')->middleware('check.permissions:settings');
     Route::get('administrative', [UserController::class, 'administrative'])->name('administrative')->middleware('check.permissions:administrative'); 
     Route::get('/batches/import', [BatchesController::class, 'import'])->name('batches.import');
@@ -47,22 +54,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/surveyphotos', [ReportsController::class, 'surveyphotos'])->name('reports.surveyphotos');
     Route::post('/surveyPhotosGenerate', [ReportsController::class, 'surveyPhotosGenerate'])->name('reports.surveyphotosgenerate');  
 });
-
-Route::get('register', [UserController::class, 'register'])->name('register');
-Route::post('register', [UserController::class, 'register_action'])->name('register.action');
-Route::get('login', [UserController::class, 'login'])->name('login');
-Route::post('login', [UserController::class, 'login_action'])->name('login.action');
-
-
-Route::get('/api/documentation', function () {
-    return view('swagger.index');
-});
-
-Route::resource('batches', BatchesController::class);
-Route::resource('clientgroups', ClientGroupController::class);
-Route::resource('clients', ClientController::class);
-Route::resource('drivers', DriverController::class);
-Route::resource('statuses', StatusController::class);
+// Route::get('/api/documentation', function () {
+//     return view('swagger.index');
+// });
 
 Route::post('/drivers/{driver}/cleartoken', [DriverController::class, 'cleartoken'])->name('drivers.cleartoken');
 Route::get('/batches/{batch}/viewuploaded', [BatchesController::class, 'viewUploaded'])->name('batches.viewuploaded');
@@ -70,10 +64,9 @@ Route::get('/batches/{batch}/upload', [BatchesController::class, 'showUploadForm
 Route::post('/batches/{batch}/upload', [BatchesController::class, 'uploadBatchDetails'])->name('batches.upload.store');
 Route::delete('/batches/{id}/details', [BatchesController::class, 'deleteBatchDetails'])->name('batches.details.delete');
 Route::get('/batches/{batch}/assigncase', [BatchesController::class, 'AssignCase'])->name('batches.assigncase');
-
 Route::post('/batches/assignbatchestodrivers', [BatchesController::class, 'assignBatchesToDrivers'])->name('batches.assignbatchestodrivers');
 
-Route::get('streetmap', [DriverController::class, 'streetmap'])->name('drivers/streetmap');
+
 
 
 
